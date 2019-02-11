8a7065b0a4b5d492569b323fc763112ecd3d5039
yesha1493 <46756394+yesha1493@users.noreply.github.com>
2019-02-10 19:22:20 -0500

    Add files via upload

---
+++ Js_MyJobs.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Js_userMaster.Master" AutoEventWireup="true" CodeBehind="Js_MyJobs.aspx.cs" Inherits="jobPortal.Js_MyJobs" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <form id="form1" runat="server">
+      
+        
+        
+        <asp:GridView  ID="GridView1" runat="server" AllowPaging="true">
+                        
+             
+        </asp:GridView>
+        <asp:SqlDataSource ID="Post" runat="server"></asp:SqlDataSource>
+    </form>
+</asp:Content>
---
+++ Js_MyJobs.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace jobPortal
+{
+    public partial class Js_MyJobs : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("Data Source=LAPTOP-LNMDAT9H\\SQLSERVER;Initial Catalog=JobPortal;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        private void BindGrid()
+        {
+            string samas2 = Session["JsUser"].ToString();
+            con.Open();
+            string sQuery = "SELECT PostHead,Descr,JobType FROM apj where id='"+samas2+"'";
+            SqlCommand cmd = new SqlCommand(sQuery, con);
+            SqlDataReader sdr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(sdr);
+            GridView1.DataSource = dt;
+            GridView1.DataKeyNames = new string[] { "PostHead" };
+            GridView1.DataBind();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["JsUser"] == null)
+            {
+                Response.Redirect("Js_Login.aspx");
+            }
+            else
+            {
+                BindGrid();
+             //   Lab1.Text = Session["JsUser"].ToString();
+            }
+
+        }
+    }
+}
