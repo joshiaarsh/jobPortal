9efce9d05d011ed159e90c64d507511b2555c316
hiral1213 <38732819+hiral1213@users.noreply.github.com>
2019-02-18 22:26:30 -0500

    Add files via upload

---
+++ Company_listPost.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/CUserMaster.Master" AutoEventWireup="true" CodeBehind="Company_listPost.aspx.cs" Inherits="jobPortal.Company_listPost" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <form runat="server">
+        <div class="col-sm-2"></div>
+        <div class="col-lg-8">
+            <div class="box">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Post List</h3>
+                </div>
+                <div class="box-body">
+                    <asp:GridView ID="gdPost" runat="server" AllowPaging="true"
+                        CssClass="table table-bordered"
+                        DataKeyNames="PostId"
+                        AutoGenerateColumns="False"
+                        OnRowDeleting="gdPost_RowDeleting"
+                        OnRowEditing="gdPost_RowEditing"
+                        OnSelectedIndexChanging="gdPost_SelectedIndexChange">
+                        <Columns>
+                            <asp:BoundField DataField="PostId" HeaderStyle-CssClass="hide" ItemStyle-CssClass="hide"></asp:BoundField>
+                            <asp:BoundField DataField="Header" HeaderText="Header"></asp:BoundField>
+                            <asp:BoundField DataField="Status" HeaderText="Status"></asp:BoundField>
+                            <asp:CommandField EditText="Update" HeaderText="Update" ShowEditButton="True"></asp:CommandField>
+                            <asp:CommandField HeaderText="Delete" ShowDeleteButton="True"></asp:CommandField>
+                            <asp:CommandField HeaderText="View" SelectText="View" ShowSelectButton="True"></asp:CommandField>
+                        </Columns>
+                    </asp:GridView>
+                </div>
+                <div>
+                    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="True" DataKeyNames="PostId">
+                        <Columns>
+                            <asp:TemplateField>
+                                <ItemTemplate>
+                                    <asp:CheckBox ID="chkSelect" runat="server" Checked='<%# Eval("Status") %>' />
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:BoundField DataField="PostHead" HeaderText="Job Post Filter" ItemStyle-Width="150px" />
+                        </Columns>
+                    </asp:GridView>
+
+                </div>
+
+                <%--     <div>
+    <%--<asp:CheckBoxList ID="CheckBoxList1" runat="server" DataSourceID="JobPortal_DS" DataTextField="PostHead" DataValueField="PostHead" AutoPostBack="True">
+        <asp:ListItem></asp:ListItem>
+        <asp:ListItem></asp:ListItem>
+        <asp:ListItem></asp:ListItem>
+        <asp:ListItem></asp:ListItem>
+                    </asp:CheckBoxList>--%>
+                <%--<asp:SqlDataSource ID="JobPortal_DS" runat="server" ConnectionString="<%$ ConnectionStrings:JobPortalConnectionString %>" SelectCommand="SELECT [PostHead], [Loc], [Salary], [JobType], [JobField] FROM [JobPost]"></asp:SqlDataSource>--%>
+            </div>
+
+            <div class="col-sm-2">
+            </div>
+    </form>
+</asp:Content>
---
+++ Company_listPost.aspx.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace jobPortal
+{
+    public partial class Company_listPost : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-VOEEFUJ\\MSSQLSERVER2017;Initial Catalog=JobPortal;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        private void BindGrid()
+        {
+            con.Open();
+            string sQuery = "SELECT PostId,PostHead as Header,case when Status = 1 then 'Active' when Status = 0 then 'Deactive' else 'UNDEFINED' end AS Status FROM JobPost where CId=9";
+            SqlCommand cmd = new SqlCommand(sQuery, con);
+            SqlDataReader sdr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(sdr);
+            gdPost.DataSource = dt;
+            gdPost.DataKeyNames = new string[] { "PostId" };
+            gdPost.DataBind();
+
+
+
+
+			using (SqlCommand cmd1 = new SqlCommand("SELECT [PostId],[PostHead], [Loc], [Salary], [JobType], [JobField],[Status] FROM[JobPost]"))
+			{
+				using (SqlDataAdapter sda = new SqlDataAdapter())
+				{
+					cmd1.Connection = con;
+					sda.SelectCommand = cmd1;
+					using (DataTable dt1 = new DataTable())
+					{
+						sda.Fill(dt1);
+						GridView1.DataSource = dt1;
+						GridView1.DataBind();
+					}
+				}
+			}
+
+		}
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["user"] == null && Session["company"] == null)
+            {
+                Response.Redirect("Company_userLogin.aspx");
+            }
+            else
+            {
+                BindGrid();
+            }
+        }
+       
+        protected void gdPost_SelectedIndexChange(object sender, GridViewSelectEventArgs e)
+        {
+            int id = e.NewSelectedIndex;
+            string getpostid = gdPost.Rows[id].Cells[0].Text;
+            Response.Redirect("Company_ViewPost.aspx?id=" + Convert.ToInt16(getpostid));
+        }
+
+        protected void gdPost_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+
+            int id = e.NewEditIndex;
+            string getpostid = gdPost.Rows[id].Cells[0].Text;
+            Response.Redirect("Company_EditPost.aspx?id=" + Convert.ToInt16(getpostid));
+
+        }
+
+        protected void gdPost_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int id = e.RowIndex;
+            string getpostid = gdPost.Rows[id].Cells[0].Text;
+
+            string s = "Update JobPost set Status= 0 where PostId=" + Convert.ToInt16(getpostid);
+            SqlCommand cmd = new SqlCommand(s, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            BindGrid();
+        }
+		
+		public DataTable GetDataTable(string field)
+		{
+			string query = string.Format("SELECT DISTINCT {0} FROM Customers", field);
+
+			String ConnString = ConfigurationManager.ConnectionStrings["NorthwindConnectionString"].ConnectionString;
+			SqlConnection conn = new SqlConnection(ConnString);
+			SqlDataAdapter adapter = new SqlDataAdapter();
+			adapter.SelectCommand = new SqlCommand(query, conn);
+
+			DataTable myDataTable = new DataTable();
+
+			conn.Open();
+			try
+			{
+				adapter.Fill(myDataTable);
+			}
+			finally
+			{
+				conn.Close();
+			}
+
+			return myDataTable;
+		}
+	}
+}
