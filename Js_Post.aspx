c08ec706caf59aaf9540413791a37da03ae9550e
hiral1213 <38732819+hiral1213@users.noreply.github.com>
2019-03-12 16:00:49 -0400

    Add files via upload

---
+++ Js_Post.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Js_userMaster.Master" AutoEventWireup="true" CodeBehind="Js_Post.aspx.cs" Inherits="jobPortal.Js_Post" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <form runat="server">
+        <div class="col-sm-2"></div>
+        <div class="col-lg-8">
+            <div class="box">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Post List</h3>
+                </div>
+                <div class="box-body">
+                    <asp:GridView ID="gdPost" runat="server" AllowPaging="true"
+                        CssClass="table table-bordered"
+                        DataKeyNames="PostId"
+                        AutoGenerateColumns="False"
+                        OnRowCommand="gdPost_RowCommand">
+                        <Columns>
+                            <asp:BoundField DataField="PostId" HeaderStyle-CssClass="hide" ItemStyle-CssClass="hide"></asp:BoundField>
+                            <asp:BoundField DataField="Header" HeaderText="Header"></asp:BoundField>
+                            <asp:BoundField DataField="Descr" HeaderText="Description"></asp:BoundField>
+                            <asp:TemplateField>
+                                <ItemTemplate>
+                                    <asp:LinkButton ID="btnApply" runat="server" CommandName="Apply" HeaderText="Apply for job" SelectText="Apply">Apply</asp:LinkButton>
+                                    <asp:LinkButton ID="btnView" runat="server" CommandName="View" HeaderText="View details" SelectText="View">View</asp:LinkButton>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                        </Columns>
+                    </asp:GridView>
+                </div>
+                <div>
+                    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="True" DataKeyNames="PostId">
+                        <Columns>
+                            <asp:TemplateField>
+                                <ItemTemplate>
+                                    <asp:CheckBox ID="chkSelect" runat="server" Checked='<%# Eval("Status") %>' />
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:BoundField DataField="PostHead" HeaderText="Job Post Filter" ItemStyle-Width="150px" />
+                        </Columns>
+                    </asp:GridView>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-sm-2">
+        </div>
+    </form>
+
+</asp:Content>
---
+++ Js_Post.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace jobPortal
+{
+    public partial class Js_Post : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-VOEEFUJ\\MSSQLSERVER2017;Initial Catalog=JobPortal;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        private void BindGrid()
+        {
+            con.Open();
+			// string sQuery = "SELECT PostId,PostHead as Header,Descr FROM JobPost" ;
+			string sQuery = "SELECT PostId,PostHead as Header,Descr,case when Status = 1 then 'Active' when Status = 0 then 'Deactive' else 'UNDEFINED' end AS Status FROM JobPost where CId=9";
+			//	"" + Session["Cid"] + " ";
+			SqlCommand cmd = new SqlCommand(sQuery, con);
+            SqlDataReader sdr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(sdr);
+            gdPost.DataSource = dt;
+            gdPost.DataKeyNames = new string[] { "PostId" };
+            gdPost.DataBind();
+
+
+			using (SqlCommand cmd1 = new SqlCommand("SELECT [PostId],[PostHead], [Loc], [Salary], [JobType], [JobField],[Status] FROM[JobPost]"))
+			{
+				using (SqlDataAdapter sda = new SqlDataAdapter())
+				{
+					cmd1.Connection = con;
+					sda.SelectCommand = cmd1;
+					using (DataTable dt1 = new DataTable())
+					{
+						sda.Fill(dt1);
+						GridView1.DataSource = dt1;
+						GridView1.DataBind();
+					}
+				}
+			}
+
+		}
+	
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        //    if (Session["JsUser"] == null)
+        //    {
+        //        Response.Redirect("Js_Login.aspx");
+        //    }
+        //    else
+        //    {
+                BindGrid();
+            //}
+        }
+
+        protected void gdPost_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Apply")
+            {
+                GridViewRow gvr = (GridViewRow)(((LinkButton)e.CommandSource).NamingContainer);
+
+                int id = gvr.RowIndex;
+                string getpostid = gdPost.Rows[id].Cells[0].Text;
+                Response.Redirect("Js_Apply.aspx?id=" + Convert.ToInt16(getpostid));
+            }
+            else if (e.CommandName == "View")
+            {
+               // int id = Convert.ToInt16(e.CommandArgument);
+                GridViewRow gvr = (GridViewRow)(((LinkButton)e.CommandSource).NamingContainer);
+
+                int id = gvr.RowIndex;
+                string getpostid = gdPost.Rows[id].Cells[0].Text;
+                Response.Redirect("Js_ViewPost.aspx?id=" + Convert.ToInt16(getpostid));
+            }
+        }
+    }
+}
